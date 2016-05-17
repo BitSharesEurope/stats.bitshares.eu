03c28a00c0bd5f71319e60ec90f8ca3cfd729e60
Fabian Schuh <mail@xeroc.org>
2016-05-17 09:25:15 +0200

    [Deploy] Customization for deployment

--- app.py
+++ app.py
@@ -17,24 +17,9 @@ def index():
     return render_template('index.html')
 
 
-@socketio.on('my event', namespace='/status')
-def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': message['data'], 'count': session['receive_count']})
-
-
-@socketio.on('my broadcast event', namespace='/status')
-def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
-
-
 @socketio.on('connect', namespace='/status')
 def test_connect():
-    emit('my response', {'data': 'Connected', 'count': 0})
+    print('Client connected', request.sid)
 
 
 @socketio.on('disconnect', namespace='/status')
@@ -44,8 +29,10 @@ def test_disconnect():
 
 @socketio.on('join', namespace='/status')
 def on_join(room):
+    # Leave all rooms!
     for r in rooms:
         leave_room(r)
+    # Join only one room
     join_room(room)
 
 
